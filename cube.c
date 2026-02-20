ecfa3726865b6eb2c6d44edb74d41b2f20bbf96a
nida <nida.dursunoglu@gmail.com>
2026-02-20 16:49:24 +0300

    Rotation is now centered by the cube

--- cube.c
+++ cube.c
@@ -5,14 +5,13 @@
 #include "cube.h"
 
 
-void printCube(const struct coordinate cubePosition, const int radius, const double zAngle,const double yAngle, const double xAngle, const struct color *colors)
+int printCube(const struct coordinate cubePosition, const int radius, const double zAngle,const double yAngle, const double xAngle, const struct color *colors)
 {
    const double cosz = cos(zAngle);
    const double sinz = sin(zAngle);
    const double cosx = cos(xAngle);
    const double sinx = sin(xAngle);
-   const double sinxdivcosx = sinx/ cosx;
-   const double sinxsqdivcosx = sinx*sinxdivcosx;
+   const double sinxDividedbyCosx = sinx/cosx;
    for(int y =0; y<heightCanvas; y++)
    {
     for(int x = 0; x<widthCanvas; x++){
@@ -24,8 +23,7 @@ void printCube(const struct coordinate cubePosition, const int radius, const dou
      pointPosition.x = tx*cosz - ty*sinz;
      pointPosition.y = tx*sinz + ty*cosz;
      //Rotate x axis
-     pointPosition.z = -pointPosition.y*sinxdivcosx;
-     pointPosition.y = pointPosition.y*cosx + pointPosition.y*sinxsqdivcosx;
+     pointPosition.y = pointPosition.y*cosx -(radius-pointPosition.y*sinx)*sinxDividedbyCosx;
      struct color pointColor = colors[0];
      if(fabs(pointPosition.x)<radius+EPS && fabs(pointPosition.y)<radius+EPS)
       {
@@ -37,13 +35,14 @@ void printCube(const struct coordinate cubePosition, const int radius, const dou
     }
     printf("\n");
    }
+   return 0;
 }
 
 void rotatingCube(const struct coordinate cubePosition, const int radius, const struct color *colors){
-    struct timespec remaining, request = { 0, 500000000 };
+    struct timespec remaining, request = { 0, 200000000 };
     int i = 0;
     while(1){
-      printCube(cubePosition, radius, 0, 0,(i-16)*M_PI/8, colors);
+      printCube(cubePosition, radius, 0, 0,(i-16)*M_PI/16, colors);
       nanosleep(&request, &remaining);
       printf("\x1b[H\x1b[J");
       i = (i + 1) % 33;
