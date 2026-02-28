cb33d137d4fc4d4093a08f4b6b00db1afd7bebfe
nida <nida.dursunoglu@gmail.com>
2026-02-28 22:49:27 +0300

    left and right faces missing

--- cube.c
+++ cube.c
@@ -7,6 +7,9 @@
 
 int printCube(const struct coordinate cubePosition, const int radius, const double zAngle,const double yAngle, const double xAngle, const struct color *colors)
 {
+   double currentz;
+   struct color pointColor;
+   bool pointExists;
    const double cosz = cos(zAngle);
    const double sinz = sin(zAngle);
    for(int y =0; y<heightCanvas; y++)
@@ -20,22 +23,30 @@ int printCube(const struct coordinate cubePosition, const int radius, const doub
      double ny = tx*sinz + ty*cosz;
      tx=nx;
      ty=ny;
-     int currentz=-100000;
-     struct color pointColor;
-     bool pointExists;
-     for (int i=1; i<2;i++) {
-       //Find the z value where after the x and y axis rotations it lands on the face
+     double currentz=-INFINITY;
+     pointExists = false;
+
+     //Front bottom back top faces all 4
+     for (int i=0; i<4;i++) {
+       //Find the z value where after the x  axis rotation it lands on the face
        double tz = (((radius-ty*sin(i*(M_PI_2) + xAngle))/cos(i*(M_PI_2) + xAngle)));
        //Rotate x axis according to this z value
        ny = ty*cos(i*(M_PI_2)+xAngle) -tz*sin(i*(M_PI_2)+xAngle);
 
-       if(fabs(nx)<radius+2*EPS && fabs(ny)<radius+2*EPS)
+       if(fabs(nx)<radius+EPS && fabs(ny)<radius+EPS && tz > currentz) 
         {
-         printf("\033[38;2;%d;%d;%dm@@\033[0m",pointColor.r,pointColor.g,pointColor.b);
+         currentz = tz;
+         pointExists =true;
+         pointColor=colors[i];
         }
-       else{
-          printf("  ");
-        }
+      }
+      
+      if(pointExists)
+      {
+        printf("\033[38;2;%d;%d;%dm@@\033[0m",pointColor.r,pointColor.g,pointColor.b);
+      }
+      else{
+        printf("  ");
       }
       
     }
@@ -48,10 +59,10 @@ void rotatingCube(const struct coordinate cubePosition, const int radius, const 
     struct timespec remaining, request = { 0, 200000000 };
     int i = 0;
     while(1){
-      printCube(cubePosition, radius, 0, 0,(i-16)*M_PI/16, colors);
+      printCube(cubePosition, radius, 0, 0,(i)*M_PI/16, colors);
       nanosleep(&request, &remaining);
       printf("\x1b[H\x1b[J");
-      i = (i + 1) % 33;
+      i = (i+ 1 )%33;
     }
 }
 
